9adfec596b631466fcbd24982e685a49a1de4dcd
Eric Lang <ealang@gmail.com>
2019-02-02 11:46:11 -0500

    Allow operation when in a subdirectory of a git repo
    Merge/rebase/etc. cases not updated

--- lib/branch.sh
+++ lib/branch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
-FILES="$(git diff $(git merge-base origin/HEAD HEAD 2> /dev/null || (git remote set-head origin -a > /dev/null && git merge-base origin/HEAD HEAD)).. --name-only)"
-if [ -n "$FILES" ]; then
-  echo "$FILES" | sort -u | xargs find 2> /dev/null || true
+GITROOT=$(git rev-parse --show-toplevel)
+FILES_GIT_REL="$(git diff $(git merge-base origin/HEAD HEAD 2> /dev/null || (git remote set-head origin -a > /dev/null && git merge-base origin/HEAD HEAD)).. --name-only)"
+FILES_ABS=$(cd $GITROOT && realpath $FILES_GIT_REL)
+FILES_PWD_REL=$(realpath --relative-to=$(pwd) $FILES_ABS)
+if [ -n "$FILES_PWD_REL" ]; then
+  echo "$FILES_PWD_REL" | sort -u | xargs find 2> /dev/null || true
 fi
--- lib/modified.sh
+++ lib/modified.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -e
-if [ -e .git/MERGE_HEAD ] || [ -d .git/rebase-merge ] || [ -d .git/rebase-apply ] || [ -e .git/CHERRY_PICK_HEAD ]; then
+GITROOT=$(git rev-parse --show-toplevel)
+if [ -e $GITROOT/.git/MERGE_HEAD ] || [ -d $GITROOT/.git/rebase-merge ] || [ -d $GITROOT/.git/rebase-apply ] || [ -e $GITROOT/.git/CHERRY_PICK_HEAD ]; then
   git status --porcelain -z | tr '\0' '\n' | grep '^[^?][^ ?] ' | cut -d ' ' -f 2-
 else
-  FILES="$(git status --porcelain -z --untracked-files=all | tr '\0' '\n')"
-  if [ -n "$FILES" ]; then
-    echo "$FILES" | cut -c 4- | tr '\n' '\0' | xargs -0 find 2> /dev/null || true
+  FILES_GIT_REL="$(git status --porcelain -z --untracked-files=all | tr '\0' '\n' | cut -c 4-)"
+  FILES_ABS=$(cd $GITROOT && realpath $FILES_GIT_REL)
+  FILES_PWD_REL=$(realpath --relative-to=$(pwd) $FILES_ABS)
+  if [ -n "$FILES_PWD_REL" ]; then
+    echo "$FILES_PWD_REL" | tr '\n' '\0' | xargs -0 find 2> /dev/null || true
   fi
 fi
